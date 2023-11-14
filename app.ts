d84e3196b7e1e3a183e65c86ae9eb77b565a092e
YesayaS <43375421+YesayaS@users.noreply.github.com>
2023-11-14 10:45:03 +0700

    feat: Add router for api and auth

--- app.ts
+++ app.ts
@@ -4,7 +4,8 @@ import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 
-import indexRouter from "./routes/index";
+import apiRouter from "./routes/api";
+import authRouter from "./routes/auth";
 
 var app = express();
 
@@ -18,7 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+app.use("/api", apiRouter);
+app.use("/auth", authRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req: Request, res: Response, next) {
