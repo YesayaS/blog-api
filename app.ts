201d79c0e8362ed5248254964ff2eb6142ff09e2
YesayaS <43375421+YesayaS@users.noreply.github.com>
2023-11-14 15:06:05 +0700

    feat: Add dotenv and errorHandler

--- app.ts
+++ app.ts
@@ -4,6 +4,9 @@ import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 
+require("dotenv").config();
+
+import * as errorHandler from "./lib/errorHandler";
 import apiRouter from "./routes/api";
 import authRouter from "./routes/auth";
 
@@ -32,15 +35,8 @@ app.use(function (req: Request, res: Response, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function (err: any, req: Request, res: Response, next: NextFunction) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+app.use(errorHandler.errorLogger);
+app.use(errorHandler.errorResponder);
+app.use(errorHandler.invalidPathHandler);
 
 export default app;
