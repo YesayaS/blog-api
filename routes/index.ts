933cbe13656c449d0b880e54a6e0e9e7afaf09ae
YesayaS <43375421+YesayaS@users.noreply.github.com>
2023-11-14 15:08:13 +0700

    refactor: Remove file

--- routes/index.ts
+++
@@ -1,9 +0,0 @@
-import express, { Request, Response, NextFunction } from 'express';
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({msg:"hi there!"});
-});
-
-export default router
